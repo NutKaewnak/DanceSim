f6a228a4475633fa907d94a40221740c6ce02208
Kittinan Napapongsa <kitty_npp@hotmail.com>
2017-03-06 23:55:33 +0700

    timelineV.1

---
+++ Assets/AnimController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimController : MonoBehaviour {
+	Animator anim;
+	public Scrollbar scroll;
+	public float maxlenght = 60;
+	public Text maxSec;
+	public Text curSec;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.PlayInFixedTime("movement",-1,scroll.value*maxlenght);
+		curSec.text=(scroll.value*maxlenght).ToString();
+	}
+
+	
+}
