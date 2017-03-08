637ee5a18161275d62a887ab77970222bf663f85
Nut Kaewnak <nut-boss@hotmail.com>
2017-03-08 21:12:47 +0700

    added camera buttons

---
+++ Assets/Scripts/ChangeObjectPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObjectPosition : MonoBehaviour {
+
+    public GameObject obj;
+    public Vector3 newPosition;
+    public void changeTransform()
+    {
+        this.obj.transform.position = this.newPosition;
+    }
+}
---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+    public Vector3 target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+	}
+}
