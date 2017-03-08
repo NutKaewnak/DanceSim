5b578f76a6ff314cf49757452b0a1afd755a43bb
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-03-08 21:10:14 +0700

    add updateCollider

---
+++ Assets/Scripts/Model/UpdateCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateCollider : MonoBehaviour {
+
+	public GameObject childModel;
+
+	// Use this for initialization
+	void Start () {
+//		this.GetComponent<BoxCollider>().center = new Vector3(1f, 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (childModel.transform.localPosition.y);
+		this.GetComponent<BoxCollider>().center = new Vector3(childModel.transform.localPosition.x, childModel.transform.localPosition.y + 1f, 
+															  childModel.transform.localPosition.z);
+	}
+}
