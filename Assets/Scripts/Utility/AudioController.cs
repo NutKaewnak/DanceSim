86ee9c20a3eaaa061a03bd884a02db0f3d46b1cb
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-03-14 23:56:25 +0700

    add audioController and controller dir

---
+++ Assets/Scripts/Utility/AudioController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+	AudioSource[] audioArr;
+	public float currentTime;
+
+	void Start() {
+		audioArr = this.transform.GetComponentsInChildren<AudioSource> ();
+	}
+
+	void Update() {
+		currentTime = SimController.instance.currentTime;
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			audioArr[0].time = currentTime;
+			audioArr[0].Play ();
+		}
+	}
+}
