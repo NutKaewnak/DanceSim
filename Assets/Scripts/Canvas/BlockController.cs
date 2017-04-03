2a83d09de41930583d3f73916d6908e42c3adb9e
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-04-03 12:10:32 +0700

    can drag audio block

---
+++ Assets/Scripts/Canvas/BlockController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BlockController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+	public static GameObject itemBeingDragged;
+	Vector3 startPosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData) {
+		itemBeingDragged = gameObject;
+		startPosition = transform.position;
+	}
+	#endregion
+
+	#region IDragHandler implementation
+	public void OnDrag (PointerEventData eventData) {
+		transform.position = new Vector3(Input.mousePosition.x, transform.position.y, 0);
+	}
+	#endregion
+
+	#region IEndDragHandler implementation
+	public void OnEndDrag (PointerEventData eventData) {
+		itemBeingDragged = null;
+	}
+	#endregion
+}
