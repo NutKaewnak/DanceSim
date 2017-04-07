1027c0105673ef0d1165620fae6ec00fb63ba6d1
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-04-07 21:32:42 +0700

    can play by audioBlock

--- Assets/Scripts/Canvas/BlockController.cs
+++ Assets/Scripts/Canvas/BlockController.cs
@@ -3,19 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class BlockController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+public class BlockController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	//START X = 88, END X = 453
+	//4 units = 1 secs
+	//scaleX = 0.2 => 25 secs
+	const float pos_x = 72;
+
+	float startTime;
+	float audioPlayTime;
+	float audioLength;
+	int audioIndex = 0;
+
+//	public float scale_x = 0.2f;
+
 	public static GameObject itemBeingDragged;
 	Vector3 startPosition;
+
 	// Use this for initialization
 	void Start () {
-		
+		transform.position = new Vector3(pos_x, transform.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 //		Debug.Log (transform.position.x);
+//		Debug.Log (pos_x);
+//		Debug.Log (Mathf.Floor(transform.position.x - pos_x));
+
+		updateStartTime ();
+		updateAudioLength ();
+		commandAudio ();
+		Debug.Log ("startTime: " + startTime);
+		Debug.Log ("currentTime: " + SimController.instance.currentTime);
+//		Debug.Log (audioLength);
+//		Debug.Log (transform.position.x);
+//		Debug.Log (transform.localScale.x);
 	}
 
+	void commandAudio () {
+		if (startTime <= SimController.instance.currentTime) {
+			AudioController.instance.playAtTime (audioIndex, audioPlayTime);
+		}
+	}
+
+	void updateStartTime () {
+		startTime = Mathf.Floor(transform.position.x - pos_x)/3.220293171196582f;
+	}
+
+	void updateAudioLength () {
+		audioLength = transform.localScale.x * (25/0.2f);
+	}
+
+	void setPosition_X (float x) {
+		transform.position = new Vector3(x, transform.position.y, transform.position.z);
+	}
+		
 	#region IBeginDragHandler implementation
 	public void OnBeginDrag (PointerEventData eventData) {
 		itemBeingDragged = gameObject;
--- Assets/Scripts/Utility/AudioController.cs
+++ Assets/Scripts/Utility/AudioController.cs
@@ -2,33 +2,49 @@
 using System.Collections;
 
 public class AudioController : MonoBehaviour {
+	public static AudioController instance;
 	AudioSource[] audioArr;
 	public float currentTime;
 	float oldTIme;
 
 	void Start() {
+		instance = this;
 		audioArr = this.transform.GetComponentsInChildren<AudioSource> ();
 	}
 
 	void Update() {
-
 		// Update current time
-		currentTime = SimController.instance.currentTime;
-
-		if (SimController.instance.state.Equals ("play")) {
-			if (!audioArr [0].isPlaying) {
-				audioArr [0].time = currentTime;
-				audioArr [0].Play ();
-			}
-		}
-
-		if (SimController.instance.state.Equals ("pause") || SimController.instance.state.Equals ("standby")) {
-			audioArr [0].time = currentTime;
-			audioArr [0].Stop ();
-		}
+//		currentTime = SimController.instance.currentTime;
+//
+//		if (SimController.instance.state.Equals ("play")) {
+//			if (!audioArr [0].isPlaying) {
+//				audioArr [0].time = currentTime;
+//				audioArr [0].Play ();
+//			}
+//		}
+//
+//		if (SimController.instance.state.Equals ("pause") || SimController.instance.state.Equals ("standby")) {
+//			audioArr [0].time = currentTime;
+//			audioArr [0].Stop ();
+//		}
 
 		//Debug.Log ("currentTime: " + currentTime);
 		//Debug.Log ("audioTime: " + audioArr [0].time);
+	}
 
+	public void playAtTime (int index, float time) {
+		if (SimController.instance.state.Equals ("play")) {
+			if (!audioArr [index].isPlaying) {
+				Debug.Log ("play");
+				audioArr [index].time = time;
+				audioArr [index].Play ();
+			}
+		}
+	}
+
+	public void stop (int index) {
+		if (SimController.instance.state.Equals ("pause") || SimController.instance.state.Equals ("standby")) {
+			audioArr [index].Stop ();
+		}
 	}
 }
