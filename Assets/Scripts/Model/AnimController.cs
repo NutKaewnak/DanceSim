4d41d067d30ecacf0fc8fadb6b479cf1f4479ad5
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-04-17 15:58:12 +0700

    model can choose audio clip

--- Assets/Scripts/Model/AnimController.cs
+++ Assets/Scripts/Model/AnimController.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor.Animations;
 
 public class AnimController : MonoBehaviour {
-
+	
 	Animator anim;
-
 	// Use this for initialization
 	void Start () {
-		anim = this.GetComponent<Animator>();
+		anim = this.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		anim.PlayInFixedTime (0, -1, SimController.instance.getCurrentTime());
+		anim.PlayInFixedTime ("testTorNaja", 0, SimController.instance.getCurrentTime ());
 	}
 }
