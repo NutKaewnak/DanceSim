19ac5370d645d78143f70058368f764220ad7762
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-04-17 16:37:59 +0700

    can open file browser

---
+++ Assets/AssetFileBrowser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEditor;
+
+public class AssetFileBrowser : MonoBehaviour, IPointerClickHandler {
+
+	#region IPointerClickHandler implementation
+	public void OnPointerClick (PointerEventData eventData) {
+		Debug.Log ("test");
+		Debug.Log(EditorUtility.OpenFilePanel ("test", "Assets/Musics", "mp3"));
+	}
+	#endregion
+}
