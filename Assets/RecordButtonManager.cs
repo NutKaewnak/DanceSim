b9821baab88987618ce55add385edced540f5d30
Kittinan Napapongsa <kitty_npp@hotmail.com>
2017-04-20 01:07:33 +0700

    recordToAnim

---
+++ Assets/RecordButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordButtonManager : MonoBehaviour {
+
+	[SerializeField] private UnityAnimationRecorder modelRecorder;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnClick () {
+
+	}
+}
