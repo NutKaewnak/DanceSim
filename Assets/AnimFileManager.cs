25f67dc1c79c6ac1592f0247135bcc6e0dc18dc4
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-04-20 01:32:21 +0700

    add animFile and manager

---
+++ Assets/AnimFileManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimFileManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+
+	Vector3 startPosition;
+	[SerializeField] private string animPath;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	#region IBeginDragHandler implementation
+	public void OnBeginDrag (PointerEventData eventData) {
+		if (!SimController.instance.isStatePlay ()) {
+			startPosition = transform.position;
+		}
+	}
+	#endregion
+
+	#region IDragHandler implementation
+	public void OnDrag (PointerEventData eventData) {
+		if (!SimController.instance.isStatePlay ()) {
+			transform.position = new Vector3 (Input.mousePosition.x - 50f, Input.mousePosition.y - 50f, 0);
+		}
+	}
+	#endregion
+
+	#region IEndDragHandler implementation
+	public void OnEndDrag (PointerEventData eventData) {
+		if (!SimController.instance.isStatePlay ()) {
+			transform.position = startPosition;
+		}
+	}
+	#endregion
+}
---
+++ Assets/Scripts/Canvas/AssetPanelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AssetPanelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject animFile = Instantiate ((GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/AnimFile.prefab", typeof(GameObject)), this.gameObject.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void updateAnimFile () {
+
+	}
+}
