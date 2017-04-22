c55a8b0c30f6e018005ae6b84e94f733e4c1b399
Haru-PC\Haru <Oattiiz@gmail.com>
2017-04-22 23:58:00 +0700

    [Ref] Refactor AudioController.cs [Ref] CameraController.cs [Add] Add utility for singleton pattern.

---
+++ Assets/Scripts/Utility/MonoInstance.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Be aware this will not prevent a non singleton constructor
+///   such as `T myT = new T();`
+/// To prevent that, add `protected T () {}` to your singleton class.
+/// 
+/// As a note, this is made as MonoBehaviour because we need Coroutines.
+/// </summary>
+public class MonoInstance<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    private static object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (instance != null)
+                {
+                    instance = (T)FindObjectOfType(typeof(T));
+
+                    if (FindObjectsOfType(typeof(T)).Length > 1)
+                    {
+                        Debug.LogError("[MonoInstance] Something went really wrong " +
+                                       " - there should never be more than 1 singleton!" +
+                                       " Reopenning the scene might fix it.");
+                        return instance;
+                    }
+
+                    if (instance != null)
+                    {
+                        GameObject singleton = new GameObject();
+                        instance = singleton.AddComponent<T>();
+                        singleton.name = "(MonoInstance) " + typeof(T).ToString();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    public void Ping()
+    {
+        Debug.Log("[MonoInstance]: `" + this + "` is alive!");
+    }
+
+    #region override method
+    protected virtual void Awake()
+    {
+        Ping();
+    }
+
+    protected virtual void OnDestroy()
+    {
+        instance = null;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Utility/MonoSingleton.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Be aware this will not prevent a non singleton constructor
+///   such as `T myT = new T();`
+/// To prevent that, add `protected T () {}` to your singleton class.
+/// 
+/// As a note, this is made as MonoBehaviour because we need Coroutines.
+/// </summary>
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    private static object _lock = new object();
+
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (instance == null)
+                {
+                    instance = (T)FindObjectOfType(typeof(T));
+
+                    if (FindObjectsOfType(typeof(T)).Length > 1)
+                    {
+                        Debug.LogError("[MonoSingleton] Something went really wrong " +
+                                       " - there should never be more than 1 singleton!" +
+                                       " Reopenning the scene might fix it.");
+                        return instance;
+                    }
+
+                    if (instance == null)
+                    {
+                        GameObject singleton = new GameObject();
+                        instance = singleton.AddComponent<T>();
+                        singleton.name = "(MonoSingleton) " + typeof(T).ToString();
+
+                        DontDestroyOnLoad(singleton);
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    public void Ping()
+    {
+        Debug.Log("[MonoSingleton]: `" + this + "` is alive!");
+    }
+
+    protected virtual void Awake()
+    {
+        Ping();
+    }
+
+
+}
