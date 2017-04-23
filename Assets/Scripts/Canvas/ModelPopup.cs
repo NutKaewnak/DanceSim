776235a5fb87f7b0179765bed84f58e030cce57b
Kittinan Napapongsa <kitty_npp@hotmail.com>
2017-04-23 16:41:30 +0700

    move model

---
+++ Assets/Scripts/Canvas/ModelPopup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelPopup : MonoBehaviour {
+
+  public DragModel dragModel;
+	[SerializeField] 
+	private GameObject MovePopup;
+	[SerializeField] 
+	private GameObject PopUp;
+
+	public MoveModel moveModel;
+	// Use this for initialization
+
+	void OnMouseDown()
+    {
+       MovePopup.gameObject.SetActive(true);
+       PopUp.gameObject.SetActive(false);
+    }
+
+    public void enableMove (){
+      moveModel.model = dragModel;
+    }
+}
---
+++ Assets/Scripts/Canvas/MoveModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveModel : MonoBehaviour {
+
+	public DragModel model;
+	public InputField startTime;
+	public InputField stopTime;
+	public Scrollbar scroll;
+	public float maxlenght = 900;
+
+	private float startTimeToSec =0;
+	private float startMin =0;
+	private float startSec =0;
+	
+	private float stopTimeToSec =900;
+	private float stopMin =0;
+	private float stopSec =0;
+
+	void Update(){
+		dragIsEnd();
+	}
+
+	public void moveIsClicked(){
+		model.setMoved();
+
+		startMin = Mathf.Floor(float.Parse(startTime.text));
+		startSec = ((float.Parse(startTime.text))-startMin)*100;
+		startTimeToSec = startMin*60+startSec;
+		scroll.value = startTimeToSec / maxlenght;
+
+		stopMin = Mathf.Floor(float.Parse(stopTime.text));
+		stopSec = ((float.Parse(stopTime.text))-stopMin)*100;
+		stopTimeToSec = stopMin*60+stopSec;
+	}
+
+	void dragIsEnd(){
+		if(model.getCheckEndDrag()){
+			scroll.value = stopTimeToSec / maxlenght;
+		}
+	}
+}
