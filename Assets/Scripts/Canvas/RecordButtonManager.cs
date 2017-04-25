6676d5f281dd4951cda821faa7dec8ebec2a22ab
Kittinan Napapongsa <kitty_npp@hotmail.com>
2017-04-26 00:14:13 +0700

    conutdown

--- Assets/Scripts/Canvas/RecordButtonManager.cs
+++ Assets/Scripts/Canvas/RecordButtonManager.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class RecordButtonManager : MonoBehaviour {
 
 	public float countTime;
 	public bool isRecord;
 	[SerializeField]
 	private UnityAnimationRecorder recorder;
+	public Text timeCounting;
+	public GameObject CountDownPanel;
 
+	public Scrollbar scroll;
 	// Use this for initialization
 	void Start () {
 		isRecord = false;
@@ -17,6 +20,7 @@ public class RecordButtonManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		countDownRecord ();
+		setText();
 	}
 
 	void countDownRecord () {
@@ -24,6 +28,8 @@ public class RecordButtonManager : MonoBehaviour {
 		if (isRecord) {
 			if (countTime <= 0) {
 				recorder.StartRecording ();
+				CountDownPanel.gameObject.SetActive(false);
+
 			} else {
 				countTime -= Time.deltaTime;
 				countTime = Mathf.Max (0, countTime);
@@ -32,7 +38,15 @@ public class RecordButtonManager : MonoBehaviour {
 	}
 
 	public void StartRecording () {
+		countTime = 5;
 		isRecord = true;
+		CountDownPanel.gameObject.SetActive(true);
+		scroll.value = Mathf.Max(0,scroll.value - 0.00666666667f);
+
+	}
+
+	void setText(){
+		timeCounting.text = Mathf.Ceil(countTime).ToString();
 	}
 
 	public void StopRecording () {
